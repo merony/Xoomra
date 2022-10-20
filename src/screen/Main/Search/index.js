9f97e4d5b1b0ed505bfd965a3dc45c1606d5aa21
merony <merony@gmail.com>
2022-10-19 23:55:44 -0400

    NR search screen moment and date range library setuped

--- src/screen/Main/Search/index.js
+++ src/screen/Main/Search/index.js
@@ -8,12 +8,14 @@ import {
 import { useEffect, useState } from 'react';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import DateRangePicker from "react-native-daterange-picker";
 import Entype from 'react-native-vector-icons/Entypo'
 import { GoogleSocialButton } from "react-native-social-buttons";
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import React from 'react';
 import { TextInput } from 'react-native-gesture-handler';
+import moment from "moment";
 import searchSugg from '../../../data/search';
 import styles from './styles';
 
@@ -22,10 +24,20 @@ const SearchScreen = ({navigation, props}) => {
     const [inpuText, setInputText] = useState('');
 
     
-    const renderItem = ({ item }) => (
-        // <Item title={item.title} />
-        <Text>{item.description}</Text>
-      );
+    constructor(props) {
+      super(props);
+      this.state = {
+        startDate: null,
+        endDate: null,
+        displayedDate: moment(),
+      };
+    }
+  
+    setDates = (dates) => {
+      this.setState({
+        ...dates,
+      });
+    };
     
 
     return (
@@ -52,15 +64,18 @@ const SearchScreen = ({navigation, props}) => {
 
                     <Text style ={styles.locationText}>test</Text>
             </View>
-           
-            
-            
-       
-            
-            
-            )}
-           
-            />
+            )} />
+
+
+        <DateRangePicker
+          onChange={this.setDates}
+          endDate={endDate}
+          startDate={startDate}
+          displayedDate={displayedDate}
+          range
+        >
+          <Text>Click me!</Text>
+        </DateRangePicker>
       
       
           </View>
