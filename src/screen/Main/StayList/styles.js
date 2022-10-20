01f258da6f31085681a25747ea97369f81be8ce8
merony <merony@gmail.com>
2022-10-20 02:29:44 -0400

    NR search page done

--- src/screen/Main/StayList/styles.js
+++ src/screen/Main/StayList/styles.js
@@ -2,6 +2,33 @@ import { Dimensions, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
 
+
+    searchButton: {
+        backgroundColor: '#fff',
+        height: 50,
+        width: Dimensions.get('screen').width - 50,
+        borderRadius: 25,
+        marginHorizontal: 25,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        position: 'absolute',
+        paddingLeft: 20,
+        borderWidth:0.8,
+        borderColor: 'lightgrey' ,
+        top: 20,
+        zIndex: 100,
+
+     
+       
+      },
+
+      searchButtonText: {
+        fontSize: 14,
+        fontWeight: '500',
+        paddingLeft: 10,
+      },
+
  
 });
 
