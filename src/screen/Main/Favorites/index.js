e000ca2ed725c47b67632a71ebb1ccb6fd9e164d
merony <merony@gmail.com>
2022-10-21 13:32:29 -0400

    NR nav screen added

---
+++ src/screen/Main/Favorites/index.js
@@ -0,0 +1,43 @@
+import {
+    FlatList,
+    Image,
+    Pressable,
+    Text,
+    TouchableOpacity,
+    View
+} from 'react-native';
+import { useEffect, useState } from 'react';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Entype from 'react-native-vector-icons/Entypo';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import { GoogleSocialButton } from "react-native-social-buttons";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import React from 'react';
+import { TextInput } from 'react-native-gesture-handler';
+import stayFeed from '../../../data/stayFeed';
+import styles from './styles';
+
+const FavoritesScreen = ({navigation, props}) => {
+    
+  
+
+
+
+
+    return (
+
+      <View style={{flexDirection: "column"}}>
+
+       <Text>Booking Screen</Text>
+
+    </View>
+        
+       
+    );
+  };
+  
+ 
+  
+  export default FavoritesScreen;
