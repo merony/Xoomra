657ecd282a56c3c80729e262911cbf96963afab7
Rafik Zakhary <rafikzakhary@Rafiks-MacBook-Pro.local>
2022-10-21 18:01:14 -0400

    RZ
    2. Created  the following screens under Screen : Verification,Profile Input. Created  the following screens under Main : StayDetails,RequestStay,StayConfirmation,wishes,trips.Created folder User under Main and created the following Screens:ViewProfile,EditProfile,MyListings,Add Listing,EditListing,Help,Support,terms

---
+++ src/screen/Main/Trips/index.js
@@ -0,0 +1,39 @@
+import {
+    FlatList,
+    Image,
+    Pressable,
+    Text,
+    TouchableOpacity,
+    View
+} from 'react-native';
+import { useEffect, useState } from 'react';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Entype from 'react-native-vector-icons/Entypo';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import { GoogleSocialButton } from "react-native-social-buttons";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import React from 'react';
+import { TextInput } from 'react-native-gesture-handler';
+import styles from './styles';
+
+const TripsScreen = ({navigation, props}) => {
+    
+  
+
+    return (
+
+      <View style={{flexDirection: "column"}}>
+
+       <Text> Trips Screen </Text>
+
+    </View>
+        
+       
+    );
+  };
+  
+ 
+  
+  export default TripsScreen;
