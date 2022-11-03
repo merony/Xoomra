c8d01c537b93a1e670bd7042a03220df8e6590cb
Rafik Zakhary <rafikzakhary@Rafiks-MacBook-Pro.local>
2022-11-03 13:06:49 -0400

    RZ
    Created the UI for profile and when the user press save its going to save data on firestore and takes user to verification screen

--- src/screen/ProfileInput/index.js
+++ src/screen/ProfileInput/index.js
@@ -16,17 +16,97 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import React from 'react';
 import { TextInput } from 'react-native-gesture-handler';
-import styles from './styles';
+import SelectList from 'react-native-dropdown-select-list'
+import DropDownPicker from 'react-native-dropdown-picker';
+import firestore from '@react-native-firebase/firestore';
+import styles from './styles'
+import auth from '@react-native-firebase/auth'
 
 const ProfileInputScreen = ({navigation, props}) => {
     
-  
+    const [facebookLinkFromUI,setFacebookLinkFromUI] = useState("")
+    const [linkedinLinkFromUI,setlinkedinLinkFromUI] = useState("")
+    const [interestFromUI,setInterestFromUI] = useState("")
+    const [aboutYourSelfFromUI,setAboutYourSelfFromUI] = useState("")
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState([]);
+    const [items, setItems] = useState([
+      {label: 'English', value: 'English'},
+      {label: 'French', value: 'French'},
+      {label: 'Arabic', value: 'Arabic'},
+      {label: 'Chinese', value: 'Chinese'},
+      {label: 'Hindi', value: 'Hindi'},
+      {label: 'Italian', value: 'Italian'},
+      {label: 'Russian', value: 'Russian'},
+      {label: 'Persian', value: 'Persian'},
+      {label: 'Spanish', value: 'Spanish'}
+    ]);
+
+    const savePressed = () =>{
+
+      console.log("PRESSED")
+
+      firestore().collection('Profiles').add({
+        uid : auth().currentUser.uid,
+        email : auth().currentUser.email,
+        FacebookURL : facebookLinkFromUI,
+        LinkedinURL : linkedinLinkFromUI,
+        Intrests : interestFromUI,
+        aboutYourSelf : aboutYourSelfFromUI,
+        Languages : value
+        })
+
+        navigation.navigate('Verification Screen')
+    }
 
     return (
 
-      <View style={{flexDirection: "column"}}>
+      <View style={{flexDirection: "column",margin:10}}>
 
-       <Text>Profile Input Screen </Text>
+
+            <View style = {styles.formField}>
+            <Ionicons style={{ paddingVertical: 4}} name='logo-facebook' size={18} color='#283239' />
+            <TextInput placeholder='Facebook URL' style = {styles.formInput}
+              value={facebookLinkFromUI} onChangeText={setFacebookLinkFromUI}/>
+            </View>
+
+            <View style = {styles.formField}>
+            <Ionicons style={{ paddingVertical: 4}} name='logo-linkedin' size={18} color='#283239' />
+            <TextInput placeholder='Linkedin URL' style = {styles.formInput}
+              value={linkedinLinkFromUI} onChangeText={setlinkedinLinkFromUI} keyboardType={'number-pad'}/>
+            </View>
+
+            <View style = {styles.formField}>
+            <Ionicons style={{ paddingVertical: 4}} name='heart-outline' size={18} color='#283239' />
+            <TextInput placeholder='Interest' style = {styles.formInput}
+              value={interestFromUI} onChangeText={setInterestFromUI} keyboardType={'number-pad'}/>
+            </View>
+
+            <View style = {styles.formField}>
+            <Ionicons style={{ paddingVertical: 4}} name='list' size={18} color='#283239' />
+            <TextInput placeholder='About Your Self' style = {styles.formInput}
+              value={aboutYourSelfFromUI} onChangeText={setAboutYourSelfFromUI} keyboardType={'number-pad'}/>
+            </View>
+
+            
+
+            <View style={{margin:10}}>
+              <DropDownPicker
+                    open={open}
+                    value={value}
+                    items={items}
+                    setOpen={setOpen}
+                    setValue={setValue}
+                    setItems={setItems}
+                    multiple={true}
+                    min={1}
+                    placeholder={"Select Language"}
+                  />
+            </View>
+            <TouchableOpacity onPress={savePressed} style = {styles.customBTN}>
+                <Text style={styles.textBTN}>Save</Text>
+            </TouchableOpacity>
+
 
     </View>
         
--- src/screen/ProfileInput/styles.js
+++ src/screen/ProfileInput/styles.js
@@ -2,7 +2,48 @@ import { Dimensions, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
 
+    headerTitle :{
 
+        fontSize: 24,
+        fontWeight: '500',
+        color: '#030f14',
+        marginBottom: 30
+        
+    },
+
+    formField:{
+
+        flexDirection: 'row',
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+        paddingBottom: 8,
+        marginBottom : 25,
+        marginHorizontal:15
+        
+    },
+
+    formInput :{
+        backgroundColor: '#e3f3fd',
+        flex: 1,
+        paddingVertical:0
+    },
+
+    customBTN : {
+        backgroundColor: '#0999f4',
+        padding: 20,
+        borderRadius: 10,
+        marginBottom: 30,
+        marginTop: 25
+        
+    },
+
+    textBTN: {
+        textAlign : 'center',
+        fontWeight: '700',
+        fontSize: 16,
+        color: '#fff',
+        marginHorizontal:20
+    }
  
 });
 
