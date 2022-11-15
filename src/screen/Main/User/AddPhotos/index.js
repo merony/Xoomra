c041d429dc07dcace3495c89109b8934c0adc1c5
Rafik Zakhary <rafikzakhary@Rafiks-MacBook-Pro.local>
2022-11-15 16:49:46 -0500

    RZ
    Completed the add photos for listing screen, missing saving data to firestore postponed after group meeting and deciding the final data base model

--- src/screen/Main/User/AddPhotos/index.js
+++ src/screen/Main/User/AddPhotos/index.js
@@ -1,10 +1,11 @@
 import {
-    FlatList,
-    Image,
-    Pressable,
-    Text,
-    TouchableOpacity,
-    View
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  Alert,
+  View
 } from 'react-native';
 import { useEffect, useState } from 'react';
 
@@ -15,27 +16,155 @@ import { GoogleSocialButton } from "react-native-social-buttons";
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import React from 'react';
-import { TextInput } from 'react-native-gesture-handler';
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
 import styles from './styles';
+import ImagePicker from 'react-native-image-crop-picker';
+import BottomSheet from 'reanimated-bottom-sheet';
+import Animated from 'react-native-reanimated';
+
 
 const AddPhotosScreen = ({navigation, props}) => {
-    
   
+const [coverPhoto,setCoverPhoto] = useState('')
+const [listingImages,setListingImages] = useState([])
+
+
+const submit = () =>{
+  // in this part data should be saved on firestore
+}
+
+const chooseCoverPhotoPressed = () =>{
+  Alert.alert(
+    "Cover Photo",
+    "Please Select How You Want To Upload",
+    [
+      {
+        text: "Camera",
+        onPress: coverPhotoCamera,
+      },
+      {text: ""},
+      {
+        text: "Gallary",
+        onPress: coverPhotoGallary,
+      },
+    ],
+    {cancelable: true}
+  );
+
+}
+
+const chooseListingPhotosPressed = () =>{
+  Alert.alert(
+    "Listing Photos",
+    "Please Select How You Want To Upload",
+    [
+      {
+        text: "Camera",
+        onPress: listingPhotosCamera,
+      },
+      {text: ""},
+      {
+        text: "Gallary",
+        onPress: listingPhotosGallary,
+      },
+    ],
+    {cancelable: true}
+  );
+
+}
 
 
 
-    return (
+  const coverPhotoCamera = () =>{
 
-      <View style={{flexDirection: "column"}}>
+  ImagePicker.openCamera({
+    cropping: true,
+  }).then(image => {
+    setCoverPhoto(image.path)
+  });
+  }
 
-       <Text>Add Photos Screen</Text>
+  const coverPhotoGallary = () =>{
+    ImagePicker.openPicker({
+      cropping: true
+    }).then(image => {
+      setCoverPhoto(image.path)
+    });
+    }
 
-    </View>
+
+    const listingPhotosCamera = () =>{
+      ImagePicker.openCamera({
+        cropping: true,
+      }).then(image => {
         
-       
-    );
-  };
+    setListingImages([...listingImages,{
+      key:listingImages.length,
+      value:image.path
+    }])
   
- 
+      });
+      }  
   
-  export default AddPhotosScreen;
+
+  const listingPhotosGallary = () =>{
+  ImagePicker.openPicker({
+    cropping: true
+  }).then(image => {
+    setListingImages([...listingImages,{
+      key:listingImages.length,
+      value:image.path
+    }])
+  });
+  }
+
+
+
+
+return (
+
+  <View style={{flexDirection: "column"}}>
+     
+    <View style={{flexDirection:"column",margin:10,borderBottomColor:'gray',borderBottomWidth:1,marginVertical:20}}>
+          <View style={{flexDirection:'row'}}>
+
+              <View style={{flex:1,justifyContent:'flex-start'}}>
+                <Text style={{color:'#283239', fontWeight: '300'}}>Cover Photo </Text>
+              </View>
+                <TouchableOpacity onPress={chooseCoverPhotoPressed}>
+                  <Text style={{color:'#0999f4', fontWeight: '500'}}>Choose  </Text>
+                </TouchableOpacity>
+          </View>
+          <View style={{flexDirection:'row',justifyContent:'center',marginVertical:20}}>
+             {coverPhoto !== '' && <Image source={{uri:coverPhoto}} style={{  width: 250,height: 150,borderColor: 'gray',borderWidth:0.5,resizeMode:'contain'}} />}  
+          </View>
+    </View>
+
+
+      <View style={{flexDirection:"column",margin:10,borderBottomColor:'gray',borderBottomWidth:1}}>
+
+        <View style={{flexDirection:'row'}}>
+            <View style={{flex:1,justifyContent:'flex-start'}}>
+            <Text style={{color:'#283239', fontWeight: '300'}}>Listing Photos</Text>
+              </View>
+                <TouchableOpacity  onPress={chooseListingPhotosPressed}>
+                  <Text style={{color:'#0999f4', fontWeight: '500'}}> Choose </Text>
+                </TouchableOpacity>
+        </View>
+
+      <View style={{flexWrap:'wrap',flexDirection:'row',justifyContent:'flex-start',marginVertical:20,marginHorizontal:10}}>
+          {listingImages.map(item =>(
+            <View style={{padding:2}}>
+              <Image key={item.key} source={{uri:item.value}} style={{ width: 100,height: 100,borderColor: 'gray',borderWidth: 1,marginHorizontal: 3,resizeMode:'contain'}} />
+            </View>
+          ) )}
+        </View>
+      </View>
+  </View>
+      
+     
+  );
+};
+
+
+export default AddPhotosScreen;
