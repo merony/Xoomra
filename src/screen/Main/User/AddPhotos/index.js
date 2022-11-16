d76f9413ea97a9ced9d3a0f4eb3abefa1d8dbe38
merony <merony@gmail.com>
2022-11-15 22:48:39 -0500

    NR just started

--- src/screen/Main/User/AddPhotos/index.js
+++ src/screen/Main/User/AddPhotos/index.js
@@ -1,10 +1,14 @@
+import * as Progress from 'react-native-progress';
+
 import {
+    Alert,
     FlatList,
     Image,
+    Platform,
     Pressable,
     Text,
     TouchableOpacity,
-    View
+    View,
 } from 'react-native';
 import { useEffect, useState } from 'react';
 
@@ -12,17 +16,26 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import Entype from 'react-native-vector-icons/Entypo';
 import Fontisto from 'react-native-vector-icons/Fontisto';
 import { GoogleSocialButton } from "react-native-social-buttons";
+import ImagePicker from 'react-native-image-picker';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import React from 'react';
 import { TextInput } from 'react-native-gesture-handler';
+import storage from '@react-native-firebase/storage';
 import styles from './styles';
 
 const AddPhotosScreen = ({navigation, props}) => {
+
+// IMAGE IT WILL HELp store Image URI
+  const [image, setImage] = useState(null);
+  // Uploadin will keep track on upload
+  const [uploading, setUploading] = useState(false);
+  // transferred will keep track on uploading progress
+  const [transferred, setTransferred] = useState(0);
+  const [imageList, setImageList] = useState9([]),
+
     
   
-
-
 
     return (
 
