9987d5502deb841ad093f9b598d17552ec7da920
Developer-Ayan <58031124+developer-ayan@users.noreply.github.com>
2022-11-21 13:18:32 -0800

    fixed problems

--- src/components/StayDetails/index.js
+++ src/components/StayDetails/index.js
@@ -1,5 +1,5 @@
-import {AccommodationsDB, cUserDB, profilesDB, usersDB} from '../../data/firRef';
-import {Alert, Image, Modal, Pressable, ScrollView, Text, TouchableOpacity, View} from 'react-native';
+import { AccommodationsDB, cUserDB, profilesDB, usersDB } from '../../data/firRef';
+import { Alert, Image, Modal, Pressable, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { getFocusedRouteNameFromRoute, useNavigation } from '@react-navigation/native';
 import { useEffect, useState } from 'react';
 
@@ -24,11 +24,11 @@ import styles from './styles.js';
 
 
 const StayDetailsComponent = (props) => {
-  const [checkInDate,setCheckInDate] = useState(new Date())
-  const [checkOutDate,setCheckOutDate] = useState(new Date())
-  const [totalNights,setTotalNights] = useState(0)
-  const [descriptionFolded,setDescriptionFolded] = useState(true)
-  const [ruleFolded,setRuleFolded] = useState(true)
+  const [checkInDate, setCheckInDate] = useState(new Date())
+  const [checkOutDate, setCheckOutDate] = useState(new Date())
+  const [totalNights, setTotalNights] = useState(0)
+  const [descriptionFolded, setDescriptionFolded] = useState(true)
+  const [ruleFolded, setRuleFolded] = useState(true)
 
 
 
@@ -38,77 +38,79 @@ const StayDetailsComponent = (props) => {
   const dummyDataSuperHostBar = 4.5
   const dummyDataHostName = 'John'
 
-  const stays= props.stays;
-  const personalData= props.personalDatas;
-  const profileData= props.profileDatas;
+  const stays = props.stays;
+  const personalData = props.personalDatas;
+  const profileData = props.profileDatas;
   const images = stays.images;
-  const myDestination =  stays.WantToGo 
+  const myDestination = stays.WantToGo
 
 
   // console.log ('User Personal Information Data', personalData);
-  
-  console.log ('User Accodmodation Data', stays.AccommodationDetails);
-  console.log ('User Personal Data', personalData);
-  console.log ('User profile Data', profileData.overallRatings);
 
-  console.log ('User wantso to go Data', myDestination);
+  console.log('User Accodmodation Data', stays.AccommodationDetails);
+  console.log('User Personal Data', personalData);
+  console.log('User profile Data', profileData.overallRatings);
 
-  console.log ('User Pictures Data', images);
+  console.log('User wantso to go Data', myDestination);
+
+  console.log('User Pictures Data', images);
 
 
-  const data = [stays,descriptionFolded]
+  const data = [stays, descriptionFolded]
   const navi = useNavigation()
-  const onDescriptionPressed = () =>{
-    if(descriptionFolded) {
+  const onDescriptionPressed = () => {
+    if (descriptionFolded) {
       setDescriptionFolded(false)
-    }else{
+    } else {
       setDescriptionFolded(true)
     }
   }
-  const onRulePressed = () =>{
-    if(ruleFolded) {
+  const onRulePressed = () => {
+    if (ruleFolded) {
       setRuleFolded(false)
-    }else{
+    } else {
       setRuleFolded(true)
     }
   }
 
-  const onReservePressed = () =>{
-    if(totalNights>=1){
-      if(dummyDataForMaxNights>=totalNights){
+  const onReservePressed = () => {
+    if (totalNights >= 1) {
+      if (dummyDataForMaxNights >= totalNights) {
         navi.navigate('RequestStayScreen',
-        {id:stays.id,checkInDate:checkInDate,
-          checkOutDate:checkOutDate,totalNights:totalNights})
-      }else{
+          {
+            id: stays.id, checkInDate: checkInDate,
+            checkOutDate: checkOutDate, totalNights: totalNights
+          })
+      } else {
         Alert.alert('max nights exceeded')
       }
     }
-    else{
-      Alert.alert('ERROR','Stay at least for 1 night to continue')
+    else {
+      Alert.alert('ERROR', 'Stay at least for 1 night to continue')
     }
   }
-  const onHostPressed = () =>{
+  const onHostPressed = () => {
     Alert.alert('navigate to host profile')
   }
 
-  const calculateInNights = (inDate) =>{
+  const calculateInNights = (inDate) => {
     let diff = checkOutDate.getTime() - inDate.getTime()
-    let nights = Math.floor(diff / (1000*60*60*24))
-    if(nights <= 0){
+    let nights = Math.floor(diff / (1000 * 60 * 60 * 24))
+    if (nights <= 0) {
       nights = 0
     }
     setTotalNights(nights)
   }
 
-  const calculateOutNights = (outDate) =>{
+  const calculateOutNights = (outDate) => {
     let diff = outDate.getTime() - checkInDate.getTime()
-    let nights = Math.floor(diff / (1000*60*60*24))
-    if(nights < 0){
+    let nights = Math.floor(diff / (1000 * 60 * 60 * 24))
+    if (nights < 0) {
       nights = 0
     }
-    
+
     setTotalNights(nights)
-    
+
   }
 
 
@@ -118,15 +120,15 @@ const StayDetailsComponent = (props) => {
   useEffect(() => {
     //Runs on every render
 
-     
+
   });
 
 
   useEffect(() => {
     //Runs only on the first render
-    
-  
-   
+
+
+
   }, []);
 
 
@@ -135,108 +137,112 @@ const StayDetailsComponent = (props) => {
 
 
   //if rating is better than 4.5, he is a super host
-  const ReturnHostReviews = (propsR) =>{
-    const rating= propsR.rating
+  const ReturnHostReviews = (propsR) => {
+    const rating = propsR.rating
     const bar = propsR.bar
 
     let result
-    if(rating>=bar){
-      result = 
-        <View style={{flexDirection:'row',paddingTop:5}}>
-          <MaterialIcons name='star-rate' size={20} color={'#030f14'} style={{paddingLeft:10}}/>
+    if (rating >= bar) {
+      result =
+        <View style={{ flexDirection: 'row', paddingTop: 5 }}>
+          <MaterialIcons name='star-rate' size={20} color={'#030f14'} style={{ paddingLeft: 10 }} />
           <Text style={styles.stayDetailsSubTitle}>{dummyDataUserRating.toFixed(2)} </Text>
-          <Fontisto name="check" size={12} color={'#030f14'} style={{paddingLeft:20,paddingTop:5}} />
+          <Fontisto name="check" size={12} color={'#030f14'} style={{ paddingLeft: 20, paddingTop: 5 }} />
           <Text style={styles.stayDetailsSubTitle}>Superhost </Text>
         </View>
-    }else{
-      result =  
-        <View style={{flexDirection:'row',paddingTop:5}}>
-          <MaterialIcons name='star-rate' size={20} color={'#030f14'} style={{paddingLeft:10}}/>
+    } else {
+      result =
+        <View style={{ flexDirection: 'row', paddingTop: 5 }}>
+          <MaterialIcons name='star-rate' size={20} color={'#030f14'} style={{ paddingLeft: 10 }} />
           <Text style={styles.stayDetailsSubTitle}>{dummyDataUserRating.toFixed(2)} </Text>
         </View>
     }
     return result
   }
 
-    return (
-      <View style={{flexDirection:'column'}}>
-        <ScrollView style = {styles.container}>
+  const _modifiedArray = images.map((item, index) => {
+    return { img: item }
+  })
 
-          {/* image */}
-          {/* <Image style={styles.Image}source={{uri: stays.image}}/> */}
+  return (
+    <View style={{ flexDirection: 'column' }}>
+      <ScrollView style={styles.container}>
 
-         
+        {/* image */}
+        {/* <Image style={styles.Image}source={{uri: stays.image}}/> */}
 
-          <ImageSlider 
-  //   data={() =>{
-  //     const arr = []
-  //     images.map((image) => {
-  //       return arr.push({'img': image} );
-  //     }
-  //     );
-  //   }
-  // }
 
-    data={images}
-    autoPlay={false}
-    onItemChanged={(item) => console.log("item", item)}
-    closeIconColor="#fff"
-    style={styles.Image}
-/>
-          {/* title */}
-          <Text style={styles.stayDetailsTitle}>{stays.StayTitle}</Text>
-          {/* location */}
-          <Text style={styles.stayDetailsSubTitle}>{stays.City} {stays.State}</Text>
-          <Divider/>
 
-          {/* host info area*/}
-          <Pressable onPress={onHostPressed}>
-            <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}> 
-              <View>
-                {/* limits, type*/}
-                <Text style={styles.stayDetailsHostTitle}>Hosted by {personalData.firstName} {personalData.lastName}  </Text>
-                <ReturnHostReviews rating={profileData.overallRatings} bar={dummyDataSuperHostBar}/>
-              </View>
-                <View>
-                  <Image style={styles.hostImage}source={{uri: stays.image}}/>
-                </View>
+        <ImageSlider
+          //   data={() =>{
+          //     const arr = []
+          //     images.map((image) => {
+          //       return arr.push({'img': image} );
+          //     }
+          //     );
+          //   }
+          // }
+
+          data={_modifiedArray}
+          autoPlay={false}
+          onItemChanged={(item) => console.log("item", item)}
+          closeIconColor="#fff"
+          style={styles.Image}
+        />
+        {/* title */}
+        <Text style={styles.stayDetailsTitle}>{stays.StayTitle}</Text>
+        {/* location */}
+        <Text style={styles.stayDetailsSubTitle}>{stays.City} {stays.State}</Text>
+        <Divider />
+
+        {/* host info area*/}
+        <Pressable onPress={onHostPressed}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+            <View>
+              {/* limits, type*/}
+              <Text style={styles.stayDetailsHostTitle}>Hosted by {personalData.firstName} {personalData.lastName}  </Text>
+              <ReturnHostReviews rating={profileData.overallRatings} bar={dummyDataSuperHostBar} />
             </View>
-          </Pressable>
-          <Divider/>
+            <View>
+              <Image style={styles.hostImage} source={{ uri: stays.image }} />
+            </View>
+          </View>
+        </Pressable>
+        <Divider />
 
-          {/*Sleeping Arrangements area*/}
-          <Pressable >
+        {/*Sleeping Arrangements area*/}
+        <Pressable >
           <View style={styles.titleContainer}>
             <Text style={styles.stayDetailsTitle}>Sleeping Arrangements</Text>
           </View>
           <View style={styles.sleepContainer}>
             {/* type */}
-            <View style={{flexDirection:'row'}}>
-            <MaterialIcons name='local-hotel' size={20} color={'#030f14'} style={{paddingLeft:5}}/>
-            <Text style={styles.stayDetailsSubTitle}> {stays.AccommodationType}  </Text>
-            
-          </View>
-          {/* guest */}
-          <View style={{flexDirection:'row'}}>
-            <Fontisto name="person" size={18} color={'#030f14'} style={{paddingLeft:20}} />
-            <Text style={styles.stayDetailsSubTitle}>{stays.maxGuest} guests</Text>
-           
-          </View>
-          {/* nights */}
-          <View style={{flexDirection:'row'}}>
-            <Fontisto name="night-clear" size={15} color={'#030f14'} style={{paddingLeft:30,paddingTop:4}}/>
-            <Text style={styles.stayDetailsSubTitle}>{stays.maxAvailableDays} nights</Text>
-          </View>      
-          </View>
-          </Pressable>
-          <Divider/>
+            <View style={{ flexDirection: 'row' }}>
+              <MaterialIcons name='local-hotel' size={20} color={'#030f14'} style={{ paddingLeft: 5 }} />
+              <Text style={styles.stayDetailsSubTitle}> {stays.AccommodationType}  </Text>
 
-          {/* Want to go  area*/}
-          <Pressable >
-            <View style={styles.titleContainer}>
-              {/* <Fontisto name="list-2" size={18} color={'#030f14'} style={{paddingTop:5}} /> */}
-              <Text style={styles.stayDetailsTitle}>Want To Go</Text>
             </View>
+            {/* guest */}
+            <View style={{ flexDirection: 'row' }}>
+              <Fontisto name="person" size={18} color={'#030f14'} style={{ paddingLeft: 20 }} />
+              <Text style={styles.stayDetailsSubTitle}>{stays.maxGuest} guests</Text>
+
+            </View>
+            {/* nights */}
+            <View style={{ flexDirection: 'row' }}>
+              <Fontisto name="night-clear" size={15} color={'#030f14'} style={{ paddingLeft: 30, paddingTop: 4 }} />
+              <Text style={styles.stayDetailsSubTitle}>{stays.maxAvailableDays} nights</Text>
+            </View>
+          </View>
+        </Pressable>
+        <Divider />
+
+        {/* Want to go  area*/}
+        <Pressable >
+          <View style={styles.titleContainer}>
+            {/* <Fontisto name="list-2" size={18} color={'#030f14'} style={{paddingTop:5}} /> */}
+            <Text style={styles.stayDetailsTitle}>Want To Go</Text>
+          </View>
 
 
           {/* {
@@ -252,64 +258,67 @@ const StayDetailsComponent = (props) => {
                }):<Text></Text>
             } */}
 
+          <LongTextComponent dataText={myDestination?.City + ' , ' + myDestination?.State + ' , ' + myDestination?.Country} />
+
+
           <Text
-          numberOfLines={5} 
-          ellipsizeMode='tail' 
-          style={styles.stayDescription}>
-           
+            numberOfLines={5}
+            ellipsizeMode='tail'
+            style={styles.stayDescription}>
+
           </Text>
 
 
-            {/* <LongTextComponent data = {[{description:dummyDataForWantToGo},false]}/> */}
-          </Pressable>
-          <Divider/>
+          {/* <LongTextComponent data = {[{description:dummyDataForWantToGo},false]}/> */}
+        </Pressable>
+        <Divider />
 
-          {/* Accommodation Details area */}
-          <Pressable onPress={onDescriptionPressed}>
+        {/* Accommodation Details area */}
+        <Pressable onPress={onDescriptionPressed}>
 
           <View style={styles.titleContainer}>
             <Text style={styles.stayDetailsTitle}>Accommodation Details</Text>
           </View>
 
-            <LongTextComponent dataText = {stays.AccommodationDetails} dataFold = {descriptionFolded}/>
+          <LongTextComponent dataText={stays.AccommodationDetails} dataFold={descriptionFolded} />
 
 
-          </Pressable>
-          <Divider/>
+        </Pressable>
+        <Divider />
 
 
-          {/* house rules */}
-          {/* use description for now */}
-          <Pressable onPress={onRulePressed}>
+        {/* house rules */}
+        {/* use description for now */}
+        <Pressable onPress={onRulePressed}>
           <View style={styles.titleContainer}>
             {/* <Fontisto name="shield" size={25} color={'#030f14'} style={{paddingTop:5}}/> */}
             <Text style={styles.stayDetailsTitle}>House Rules</Text>
           </View>
 
-            <LongTextComponent dataText = {stays.HouseRules} dataFold = {ruleFolded}/>
-          </Pressable>
-          <Divider/>
+          <LongTextComponent dataText={stays.HouseRules} dataFold={ruleFolded} />
+        </Pressable>
+        <Divider />
 
-        </ScrollView>
+      </ScrollView>
 
-        {/* reserve area*/}
-        <View style={{flexDirection:'row',justifyContent:'space-between',width:'90%',alignItems:'center',marginLeft:20,height:70}}>
-          {/* date picker area */}
-          <View style={styles.datePickerContainer}>
-            <DatePickerComponent style = {styles.datePickerComponent} setCDate={setCheckInDate} setCNights={calculateInNights} />
-            <Text>-</Text>
-            <DatePickerCheckOutComponent style = {styles.datePickerComponent} setCDate={setCheckOutDate} setCNights={calculateOutNights}/>
-          </View>
-
-          {/* reserve button area */}
-          <Pressable style={styles.customBTN} onPress={onReservePressed}>
-            <Text style={styles.textBTN}>Exchange Request</Text>
-          </Pressable>
+      {/* reserve area*/}
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: '90%', alignItems: 'center', marginLeft: 20, height: 70 }}>
+        {/* date picker area */}
+        <View style={styles.datePickerContainer}>
+          <DatePickerComponent style={styles.datePickerComponent} setCDate={setCheckInDate} setCNights={calculateInNights} />
+          <Text>-</Text>
+          <DatePickerCheckOutComponent style={styles.datePickerComponent} setCDate={setCheckOutDate} setCNights={calculateOutNights} />
         </View>
-      </View>   
-    );
-  };
-  
- 
-  
-  export default StayDetailsComponent;
+
+        {/* reserve button area */}
+        <Pressable style={styles.customBTN} onPress={onReservePressed}>
+          <Text style={styles.textBTN}>Exchange Request</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+
+
+export default StayDetailsComponent;
